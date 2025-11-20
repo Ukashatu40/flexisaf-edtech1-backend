7fcda7b0c00821be306d4f6d6d39f5057cc5e784
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-20 18:45:16 +0100

    Initial commit

---
+++ src/main/java/com/edtech1/edtech1_backend/Edtech1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.edtech1.edtech1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edtech1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edtech1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edtech1/edtech1_backend/Edtech1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edtech1.edtech1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edtech1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
